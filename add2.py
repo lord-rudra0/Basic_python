59d602fd25d5e21591889e357e788afcce27690e
rudra pratap singh <prataprudra10102001@gmail.com>
2024-11-03 02:15:11 +0530

    Basic python

---
+++ add2.py
@@ -0,0 +1,8 @@
+#write the code to add two numbers user inputs
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum = a + b
+print("The sum of the two numbers is", sum)
+# give code to convert the sum to string and print the type of the sum
+sum = str(sum)
+print(type(sum))
